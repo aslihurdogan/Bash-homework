4e92b10d74acbbc7077bcf9782706d08bc1cf87a
EC2 Default User <ec2-user@ip-172-31-82-66.ec2.internal>
2025-06-06 00:31:05 +0000

    updates on Homework12

---
+++ Homework-12/input.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+read -p "Enter your name:" name
+read -p "Enter your age:" age
+read -p "Enter your favorite_language:" favorite_language
+read -p "Enter your hobby:" hobby
+read -p "Enter your dream:" dream 
+
+
+echo "Hi, my name is $name, and I'm $age years old. Every morning, $name likes to spend time $hobby. As a $age year-old, I believe it is never too late to follow your dreams. My favorite programming language is $favorite_language. I code in $favorite_language almost every day! One day, I hope my love for $favorite_language will help me achive my dream of $dream." 
+echo "When I am not busy working toward $dream, you will find me $hobby to relax." That's a little about me, $name - a $48 year old who loves $favorite_language, enjoys $hobby, and dreams of $dream!"
---
+++ Homework-12/introduce.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+name=Asli
+age=48
+favorite_language=linux
+hobby=reading
+dream=becoming a devops engineer
+
+echo "Hi, my name is $name, and I'm $age years old. Every morning, $name likes to spend time $hobby. As a $age year-old, I believe it is never too late to follow your dreams. My favorite programming language is $favorite_language. I code in $favorite_language almost every day! One day, I hope my love for $favorite_language will help me achive my dream of $dream." 
+echo "When I am not busy working toward $dream, you will find me $hobby to relax." That's a little about me, $name - a $48 year old who loves $favorite_language, enjoys $hobby, and dreams of $dream!"
